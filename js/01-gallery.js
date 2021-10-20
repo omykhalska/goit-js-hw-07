ad00ed5ab7a7c19b93748d1ecfff706ec39e4983
omykhalska <83216292+omykhalska@users.noreply.github.com>
2021-10-20 23:22:13 +0300

    Добавляет решение основного задания №1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+
+galleryContainer.addEventListener('click', onGalleryClick);
+
+function createGalleryMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+
+function onGalleryClick(e) {
+  e.preventDefault();
+
+  if (e.target === e.currentTarget) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src=${e.target.dataset.source} width="1280">
+`);
+
+  instance.show();
+}
